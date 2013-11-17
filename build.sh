e262125c378b53740d5db2ed022c5b8d0232aa94
C457 <android.c357@gmail.com>
2013-11-17 13:20:11 -0600

    Ramdisk

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf kernel.zip
+rm -rf ramdisk.gz
+find -name "*~" -exec rm -rf {} \;
+
+cd boot.img-ramdisk
+find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
+cd ..
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x10000000 ---ramdisk_offset 0x11000000 -o boot.img
