00052ec3ed55771da4d5b8a4e37df1fa4afbae9e
C457 <android.c357@gmail.com>
2013-11-17 13:33:37 -0600

    typo

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ find -name "*~" -exec rm -rf {} \;
 cd boot.img-ramdisk
 find . | cpio -o -H newc | gzip -9 > ../ramdisk.gz
 cd ..
-./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x10000000 ---ramdisk_offset 0x11000000 -o boot.img
+./mkbootimg --kernel zImage --ramdisk ramdisk.gz --base 0x10000000 --ramdisk_offset 0x11000000 -o boot.img
